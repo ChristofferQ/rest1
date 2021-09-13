bf38c8e27d0261410000d34206d3be27e92f8e46
Christoffer Quakernaat <cph-cq2@cphbusiness.dk>
2021-09-13 22:31:06 +0200

    Working on exercise 4

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(entity.AnimalFromDB.class);
         resources.add(rest.AnimalDemo.class);
     }
     
